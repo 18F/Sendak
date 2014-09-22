217cfb3161caa86f820d09dd55de08610976b163
jane avriette <jane@cpan.org>
2014-09-22 16:08:41 -0400

    helper code. todo: 'keys' function.

---
+++ components/common/js/display_raw.js
@@ -0,0 +1,20 @@
+// Console.log gives us json which is less-parsable for things like shell.
+// so because I, Jane, use shell A Whole Lot, I am gifting myself this for
+// later.
+//
+
+// Return a string of lines which are all the requested keys joined by a comma
+//
+var display_raw = function ( hash, keys ) {
+	var display = '';
+	var raw_display = '';
+	for (var key in keys) { // {{{
+		// if (hash.hasOwnProperty( key )) {
+		if (raw_display != '') {
+			raw_display = raw_display + ',' ;
+		}
+		raw_display = raw_display + hash[key];
+		display = raw_display + "\n"
+		raw_display = '' ; 
+	} // iterate display
+} // display_raw anon function
