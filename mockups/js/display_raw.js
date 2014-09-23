f81a310ccfa124d81df19753d5ab5aff6a9964a0
jane avriette <jane@cpan.org>
2014-09-23 12:37:37 -0400

    testign

---
+++ mockups/js/display_raw.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+// Wrapper to proof the display-raw function
+//
+
+var sendak_supplemental = require( './components/common/js/supplemental.js' );
+
+var dict = {
+	'wanted_key_a' : 'long value for a',
+	'wanted_key_b' : 'long value for b',
+
+	'unwanted_key_a' : 'long value for uwa',
+	'unwanted_key_b' : 'long value for uwb'
+};
+
