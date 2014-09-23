14a5fc2aa76c14b4c4f033a13ccb8b71c27f7bf7
jane avriette <jane@cpan.org>
2014-09-23 12:36:58 -0400

    really this makes more sense rather than having them broken out

--- components/common/js/supplemental.js
+++ components/common/js/supplemental.js
@@ -2,7 +2,6 @@
 // so because I, Jane, use shell A Whole Lot, I am gifting myself this for
 // later.
 //
-
 // Return a string of lines which are all the requested keys joined by a comma
 //
 var display_raw = function ( hash, keys ) {
@@ -19,3 +18,23 @@ var display_raw = function ( hash, keys ) {
 	} // iterate display
 	return display;
 } // display_raw anon function
+
+exports.display_raw = display_raw;
+
+
+// There are several places where I am reproducing this code which just
+// returns the keys of a dictionary/hash. I got tired of this. Basically
+// synonymous with perl's 'keys %foo' function.
+//
+
+var get_keys = function ( hash, keys ) {
+	var r_keys = [ ];
+	for (var key in keys) { // {{{
+		if (hash.hasOwnProperty( key )) {
+			r_keys.push( key );
+		}
+	} // iterate hash }}}
+	return r_keys;
+} // get_keys anon function
+
+exports.get_keys = get_keys
