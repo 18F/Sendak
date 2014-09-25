36902f6663ef3351025f60b6328e0939c9e8edde
jane avriette <jane@cpan.org>
2014-09-25 12:20:00 -0400

    incremental commit of the write method for odorm revised what the data structure actually looks like in-memory but the api calls should still be the same

--- components/odorm/odorm.js
+++ components/odorm/odorm.js
@@ -1,6 +1,10 @@
-var odorm  = require( './schema.js' );
-var schema = odorm.orm;
+// var odorm  = require( './schema.js' );
+var odorm;
+var schema = { };
+var datastore = { };
+
 var crypto = require('crypto');
+var supps  = require('components/common/js/supplemental.js');
 
 // 'schema' should be a hash that includes one hash-per-key (so, it is a hash of hashes)
 // 'callback' will get called with your fancy on-disk "orm" (see docs)
@@ -14,6 +18,43 @@ var crypto = require('crypto');
 //     callback will be passed the return from fs.writeFile.
 //
 module.exports = {
+	new_schema : function (filename) { // {{{
+		if (filename) {
+			// Define a new schema
+			//
+			odorm     = require( filename );
+			schema    = odorm.orm;
+			datastore = schema;
+
+			for (var objtype in supps.get_keys(schema)) {
+				// Don't pollute 'schema', which is what we are working off, rather
+				// create a new variable and populate that accordingly.
+				//
+				datastore[ schema[objtype] ] = [ ];
+			}
+			return datastore;
+		}
+		else {
+			// This is a request for a clean copy of the schema.
+			//
+			return schema;
+		} // filename
+	}, // }}} new_schema()
+
+	add_object : function( datastore, type, object ) { // {{{
+		if ( datastore.hasOwnProperty( type )) {
+			// This looks like a datastore that has an object of the type we are
+			// being asked to commit.
+			//
+			datastore[ type ].push( object );
+		}
+		else {
+			// We don't actually have objects of this type, so error
+			//
+			return null; // this should actually be an exception?
+		}
+	}, // }}} add_object()
+
 	new_object : function (varname) { // {{{
 		if (schema.hasOwnProperty( varname )) {
 			// Well, if we have one of those, let's clone it and send the clone back
@@ -22,7 +63,7 @@ module.exports = {
 			var map   = schema[ varname ];
 			var clone = { };
 
-			for (var property in map) {
+			for (var property in map) { // {{{
 				if ( map.hasOwnProperty( property ) && (property != 'hasone') && (property != 'hasmany') ) {
 					// Need to create a hash with key property and get a serial for it because
 					// this is a new object. XXX: this implies the schema exists somewhere
@@ -48,7 +89,7 @@ module.exports = {
 					// clone.settr_method = 
 					//
 				} // if it's a key and the right key
-			} // walk the hash
+			} // walk the hash }}}
 
 			return clone;
 		}
@@ -59,22 +100,16 @@ module.exports = {
 	}, // }}} new_object()
 
 	object_types : function () { // {{{
-		var keys = [ ];
-		for (var key in schema) {
-			if (schema.hasOwnProperty( key )) {
-				keys.push( key )
-			} // if
-		} // for
-		return keys;
+		return supps.keys( schema );
 	}, // }}} object_types()
 
-	write_data : function ( filename, vars, callback ) { // {{{
+	write_data : function ( filename, datastore, callback ) { // {{{
 		// It would be nice for the objects passed back in new_object were stored
 		// in this.stuff (or something) but for now we can just write this and
 		// use it as a placeholder
 		//
 		var fs = require( 'fs' );
-		var pass = JSON.stringify( vars );
+		var pass = JSON.stringify( datastore );
 
 		// Basically if callback never gets called, you can assume fs did its job.
 		//
