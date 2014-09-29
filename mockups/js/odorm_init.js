cd9e52710a3fdd61da3cec1256da5f8af91ff0d4
jane avriette <jane@cpan.org>
2014-09-29 08:20:33 -0400

    initial commit, now to test

---
+++ mockups/js/odorm_init.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+var ORM = require( 'components/odorm/odorm.js' ); // ORM
+
+
+var schema = ORM.new_schema( 'components/odorm/schema.js' );
+
+var metadata = ORM.new_object( 'Node' );
+
+// {"name":null,"serial":"b72b4624ac4cbf0d7374d88843edc353eba85651e3527e5c990d8e1c50cf8868","instance_id":"i-f7b8de1c","availability_zone":"us-east-1a"}
+
+var my_node = ORM.new_object( 'Node' );
+
+my_node['serial']            = 'b72b4624ac4cbf0d7374d88843edc353eba85651e3527e5c990d8e1c50cf8868';
+my_node['instance_id']       = 'i-f7b8de1c';
+my_node['availability_zone'] = 'us-east-1a';
+my_node['name']              = 'first_node';
+
+if (var r = ORM.add_object( 'Node', my_node )) {
+	// Success.
+	console.log( r );
+}
+else {
+	console.log( 'Failed to write Node ID ' + my_node['instance_id'] + ' to ORM' );
+}
+
+
