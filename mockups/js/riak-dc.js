035f200d252561cbee463d95adf00afc3056ada0
jane avriette <jane@cpan.org>
2014-10-22 10:38:42 -0400

    and this is a cleaner invocation of calling get/put tuple

--- mockups/js/riak-dc.js
+++ mockups/js/riak-dc.js
@@ -7,9 +7,11 @@ var serial = riak_dc.put_tuple( 'a_new_bucket', {
 	keyname1 : 'keyvalue1',
 	keyname2 : 'keyvalue2'
 } );
-var tuple = riak_dc.get_tuple( 'bucket', 'key' );
 
-serial.then( console.log );
-
-tuple.then( console.log )
-
+serial.then( function (tid) {
+	console.log( 'looking for ' + tid );
+	var tuple = riak_dc.get_tuple( 'bucket', tid );
+	tuple.then( function (body) {
+		console.log( 'BODY: ' + body );
+	} )
+} );
