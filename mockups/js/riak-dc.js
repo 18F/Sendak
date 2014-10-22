ddf3544b059e4824e68d4976fe8c23e449335c5e
jane avriette <jane@cpan.org>
2014-10-22 10:22:54 -0400

    hey so we're returning serials too which is special
    fetch:Sendak jane$ node mockups/js/riak-dc.js
    HyIWdBGNDxtnXzNIkLAjfDClJY
    fetch:Sendak jane$ node mockups/js/riak-dc.js
    5yGhWmU5bpUO3DKNt4uI9Nmwa3
    fetch:Sendak jane$ node mockups/js/riak-dc.js
    5RdfdxftDyal3CHJVMykm7hEjC
    fetch:Sendak jane$ node mockups/js/riak-dc.js
    IXAXmaeR7EvkDiaS0SjEu161gt

--- mockups/js/riak-dc.js
+++ mockups/js/riak-dc.js
@@ -2,10 +2,14 @@
 
 var riak_dc = require( 'components/common/js/riak-dc.js' ); // our riak synchronous wrapper
 
-var result = riak_dc.put_tuple( 'food', 'favorite', 'unagi' );
-var tuple = riak_dc.get_tuple( 'food', 'favorite' );
+// var result = riak_dc.put_tuple( 'bucket', 'key', 'value' );
+var serial = riak_dc.put_tuple( 'a_new_bucket', {
+	keyname1 : 'keyvalue1',
+	keyname2 : 'keyvalue2'
+} );
+var tuple = riak_dc.get_tuple( 'bucket', 'key' );
 
-result.then(
-	tuple.then( console.log )
-);
+serial.then( console.log );
+
+tuple.then( console.log )
 
