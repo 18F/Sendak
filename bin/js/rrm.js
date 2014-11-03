452d497500e532dff36e9af54046ea1134a7b37b
jane avriette <jane@cpan.org>
2014-11-03 16:09:26 -0500

    adds support for get_objects()

--- bin/js/rrm.js
+++ bin/js/rrm.js
@@ -10,12 +10,26 @@ var nopt = require('nopt')
 			'get-schema'      : [ Boolean, null ],
 			'object-types'    : [ Boolean, null ],
 			'describe-object' : [ String, null ],
+
+			'get-objects'     : [ Boolean, null ],
+
+			'add-object'      : [ Boolean, null ],
+			'object-type'     : [ String, null ],
+			'tuple'           : [ String, null ],
+
 			'help'            : [ Boolean, null ]
 		}
 	, description = {
 			'get-schema'      : 'Return the full (Sendak-specific) schema in Riak',
 			'object-types'    : 'List the Sendak object types',
 			'describe-object' : 'Describe a specified object from the schema',
+
+			'get-objects'     : 'Lists all the objects of a specified type in Riak, by key',
+
+			'add-object'      : 'Attempts to place an object in rrm',
+			'object-type'     : 'Used with add-object/get-objects to specify the type of object',
+			'tuple'           : 'The base64-encoded object you wish to store with add-object',
+
 			'help'            : 'Sets the helpful bit.'
 		}
 	, defaults = {
@@ -51,4 +65,25 @@ else if (parsed['describe-object']) {
 	// Describe what we know about an object's prototype
 	//
 	rrm.new_object( parsed['describe-object'] ).then( console.log );
+}
+else if (parsed['add-object']) {
+	if ((! parsed['tuple'] ) || (! parsed['object-type'])) {
+		console.log( 'Usage: ' );
+		console.log( usage );
+		process.exit( -255 ); // oops
+	}
+	
+	var tuple = new Buffer(parsed['tuple'], 'base64').toString('ascii');
+
+	// this should be a serial from Riak.
+	//
+	rrm.add_object( parsed['object-type'], tuple ); // .then( console.log );
+}
+else if (parsed['get-objects']) {
+	if (! parsed['object-type']) {
+		console.log( 'Usage: ' );
+		console.log( usage );
+		process.exit( -255 ); // oops
+	}
+	rrm.get_objects( parsed['object-type'] ).then( console.log );
 }
