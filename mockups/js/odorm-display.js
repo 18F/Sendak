5b9e0116f39bee6869399aa8ed7a4b32f11f6d51
jane avriette <jane@cpan.org>
2014-11-03 13:51:53 -0500

    remove references to previous object models

--- mockups/js/odorm-display.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var ORM = require( 'components/odorm/odorm.js' ); // ORM
-
-var schema = ORM.restore_schema( 'var/datastore.json' );
-
-var metadata = ORM.new_object( 'Node' );
-
-console.log( metadata );
--- mockups/js/odorm-slurp.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var ORM = require( 'components/odorm/odorm.js' ); // ORM
-
-var schema = ORM.restore_schema( 'var/datastore.json' );
-
-var nodes = schema['Node']['data'];
-
-console.log( nodes );
