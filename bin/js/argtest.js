8bf27361ead95751322a7c1decb91fa573ec1d26
jane avriette <jane@cpan.org>
2014-11-13 11:12:41 -0500

    obsolete

--- bin/js/argtest.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-console.log( 'original arguments: ', process.argv );
-console.log( 'fixed arguments: ', require( 'components/common/js/supplemental.js' ).fix_quoted_array( process.argv ) );
