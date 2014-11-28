b8da5ec2712e0afe2db9f194fa87e54255ecb6a0
jane avriette <jane@cpan.org>
2014-11-28 14:06:37 -0500

    bump for su-0.0.3

--- bin/js/create-sendak-project.js
+++ bin/js/create-sendak-project.js
@@ -6,31 +6,25 @@ var metadata = rrm.new_object( 'Project' );
 var parsed = require( 'sendak-usage' ).parsedown( {
 	'project-name' : {
 		'description' : 'The ssh key name (not filename) you would use to log into this node with.',
-		'long-args'   : [ 'project-name' ],
 		'type'        : [ String ]
 	},
 	'with-github-project' : {
 		'description' : 'The github project name associated with this',
 		'type'        : [ String, Array ],
-		'long-args'   : [ 'with-github-project' ]
 	},
 	'with-user' : {
 		'type'        : [ String ],
 		'description' : 'A security group or several security groups that apply to this node.',
-		'long-args'   : [ 'with-user' ]
 	},
 	'with-vpc' : {
 		'type'        : [ String ],
 		'description' : 'Specify the vpc this project should be associated with',
-		'long-args'   : [ 'with-vpc' ]
 	},
 	'create-vpc' : {
 		'description' : 'Create a VPC specifically for this project (this is an IAM call)',
 		'type'        : [ Boolean ],
-		'long-args'   : [ 'create-vpc' ]
 	},
 	'help' : {
-		'long-args'   : [ 'help' ],
 		'description' : 'Halp the user.',
 		'type'        : [ Boolean ]
 	}
