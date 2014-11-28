d6307acc0e404b96c461052b6d7e392bc9ddea90
jane avriette <jane@cpan.org>
2014-11-28 16:49:29 -0500

    add some comments so I can pick this up monday

--- bin/js/check-environment.js
+++ bin/js/check-environment.js
@@ -24,10 +24,17 @@ if (nopt['help']) {
 
 var checks = {
 	'riak' : function () {
+		// This is kind of a cop-out since it assumes that a Riak is on
+		// localhost:8098, but we will deal with extra configs later (like
+		// process.env.RIAK_HOST or something).
+		//
 		var R = require( 'riak-dc' );
 		return R.ping().then( function (r) { return r } );
 	},
 	'github' : function () {
+		// We might later want to have additional github configs, but
+		// GH_TOKEN and GH_SECRET should be set.
+		//
 		var github = require( 'github' )
 			, G = new github( {
 			version  : '3.0.0',
@@ -49,6 +56,8 @@ var checks = {
 				secret  : process.env.GH_SECRET
 			} )
 		}
+		// And of course octocat has followers.
+		//
 		return G.user.getFollowingFromUser( { 'user': 'octocat' }, function (e,r) {
 			if (e) { return false }
 			if (r) { return true }
@@ -80,13 +89,19 @@ var checks = {
 			return false;
 		} )
 
+		// XXX: This probably does not do what you think it does, Jane.
+		//
 		deferred.resolve( rvals );
 
 		return deferred.promise;
 
 	},
 	'rrm' : function () {
+		// XXX: rrm has no ping() or anything. Probably best to just check for
+		//      rrm.get_object_types() -- which checks for an extant schema.
+		//
 
+		// TODO: writeme
 	}
 };
 
