54f936ddcb3f7c1bd731e1869cc5967e4124bce9
jane arc <jane@cpan.org>
2015-01-07 10:41:19 +0700

    works better than before but not finished

--- bin/js/list-sendak-users.js
+++ bin/js/list-sendak-users.js
@@ -1,18 +1,9 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict';
 
-/*
-  Simply report on the users in Riak.
-
-*/
-
-// XXX: clearly this is 107% broken/not-finished. fixy.
-
-// parse opts
-//
 var parsed = require( 'sendak-usage' ).parsedown( {
-	'username' : {
+	'user-name' : {
 		'type'        : [ String ],
 		'description' : 'Specify an expression to match against username',
 	},
@@ -20,9 +11,9 @@ var parsed = require( 'sendak-usage' ).parsedown( {
 		'type'        : [ String ],
 		'description' : 'Specify an expression to match against the arn',
 	},
-	'amznid' : {
+	'user-id' : {
 		'type'        : [ String ],
-		'description' : 'Specify an expression to match against the amznid',
+		'description' : 'Specify an expression to match against the user-id',
 	},
 	'help' : {
 		'description' : 'Halp the user.',
@@ -33,7 +24,7 @@ var parsed = require( 'sendak-usage' ).parsedown( {
 	, nopt  = parsed[0];
 
 
-if (nopt['help']) {
+if (nopt['help'] || (Object.keys(nopt).length == 0)) {
 	// Be halpful
 	//
 	console.log( 'Usage: ' );
@@ -45,10 +36,17 @@ var rrm = require( 'rrm' );
 
 var results = [ ];
 
-var pusers = rrm.get_objects( 'User' ).then( function ( userids ) {
-	if (nopt['name']) {
-		// push matching records into results
-		//
-	}
-	console.log( 'userids: ', userids )
+// This is a little buggy and doesn't always display the requested stuff
+//
+var pusers = rrm.get_objects( 'user' ).then( function ( users ) {
+	users.forEach( function (user) {
+		var json = JSON.parse( user );
+		var result = { };
+		Object.keys( nopt ).forEach( function (req_key) {
+			if (json[req_key]) { result[req_key] = json[req_key] }
+		} )
+		results.push( result )
+	} )
+	console.log( results );
 } );
+
