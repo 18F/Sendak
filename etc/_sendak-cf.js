e6a679ebc0ab7563d163f06a29ffd3f21809674c
jane arc <jane@cpan.org>
2015-02-05 08:18:11 -0500

    laying groundwork for plugsuit

---
+++ etc/_sendak-cf.js
@@ -0,0 +1,15 @@
+// base configuration for sendak
+//
+
+'use strict;'
+
+var global = { };
+
+global.logger = require( 'etc/log4js.js' ).getlogger();
+
+module.exports = {
+	getcf:     function () { return global },
+	getlogger: function () { return global.logger }
+}
+
+// jane@cpan.org // vim:tw=80:ts=2:noet
